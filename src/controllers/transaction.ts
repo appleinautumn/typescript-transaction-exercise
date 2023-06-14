f7ebc6ee667a4efb2e0fb20b0e24d945f6063230
yu <mail@firdaus.org>
2023-06-15 00:45:35 +0700

    Add transaction controller

---
+++ src/controllers/transaction.ts
@@ -0,0 +1,23 @@
+// import axios from 'axios';
+import { RequestHandler } from 'express';
+import _ from 'lodash';
+
+import { TransactionDatabase, getData } from '../db';
+import { Transaction } from '../models/transaction';
+import { parseCsvString } from '../util/csv';
+
+async function getData(): Promise<string> {
+  const dataUrl =
+    'https://raw.githubusercontent.com/bunker-tech/take-home-exercises/main/shared/sample_transactions.csv';
+
+  const { data, status } = await axios({
+    method: 'get',
+    url: dataUrl,
+  });
+
+  return data;
+}
+
+export const listTransactions: RequestHandler = async (req, res, next) => {
+  res.json(data);
+};
