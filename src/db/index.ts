d274732b15d297423f21d8b56bf53f94583612dd
yu <mail@firdaus.org>
2023-06-15 00:54:38 +0700

    On app started, load the csv to memory

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,31 @@
 import fs from 'fs';
 import * as csv from 'fast-csv';
 import { ParserOptionsArgs } from 'fast-csv';
+import { HeaderArray, HeaderTransformFunction } from '@fast-csv/parse';
 import * as path from 'path';
 
 import { Transaction } from '../models/transaction';
 
-let data: Transaction[];
+export interface TransactionDatabase<T> {
+  [key: string]: T;
+}
+
+// create a hashtable to simulate the database
+const _db: TransactionDatabase<Transaction> = {};
+
+export function getData() {
+  return _db;
+}
 
 export async function loadDatabase() {
   const options: ParserOptionsArgs = {
     objectMode: true,
     delimiter: ',',
     quote: '"',
-    headers: true,
-    renameHeaders: false,
+    headers: (headerArray) =>
+      headerArray.map((header: string | null | undefined) =>
+        header?.toLowerCase()
+      ),
   };
 
   fs.createReadStream(path.resolve(__dirname, '../../data', 'data.csv'))
@@ -22,9 +34,16 @@ export async function loadDatabase() {
       console.log(error);
     })
     .on('data', (row) => {
-      data.push(row);
+      // make id integer
+      const id = Number(row.id);
+
+      // make tags an array
+      const tags: string[] = row.tags.split(',');
+      row.tags = tags.map((el) => el.trim());
+
+      _db[id] = row;
     })
     .on('end', (rowCount: number) => {
-      return _db;
+      // console.log(rowCount);
     });
 }
