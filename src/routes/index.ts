a2c4567d19a34f43766941e3ed62d580144ff052
yu <mail@firdaus.org>
2023-06-15 00:46:15 +0700

    Add list and get route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,4 +4,8 @@ import { getTransaction, listTransactions } from '../controllers/transaction';
 
 const router = Router();
 
+router.get('/:id', getTransaction);
+
+router.get('/', listTransactions);
+
 export default router;
