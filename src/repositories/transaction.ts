e6c6be6161d70ad33f25cda25a3e84d302302fc2
yu <mail@firdaus.org>
2023-06-15 13:02:00 +0700

    Add repository

---
+++ src/repositories/transaction.ts
@@ -0,0 +1,90 @@
+import fs from 'fs';
+import * as csv from 'fast-csv';
+import { ParserOptionsArgs } from 'fast-csv';
+import { HeaderArray, HeaderTransformFunction } from '@fast-csv/parse';
+import * as path from 'path';
+
+import { Transaction, TransactionDatabase } from '../models/transaction';
+import { clearLine } from 'readline';
+
+// create a hashtable to simulate the database
+// const _db: TransactionDatabase<Transaction> = {};
+
+// export function getData() {
+//   return _db;
+// }
+
+// async function loadDatabase() {
+//   const options: ParserOptionsArgs = {
+//     objectMode: true,
+//     delimiter: ',',
+//     quote: '"',
+//     headers: (headerArray) =>
+//       headerArray.map((header: string | null | undefined) =>
+//         header?.toLowerCase()
+//       ),
+//   };
+
+//   fs.createReadStream(path.resolve(__dirname, '../../data', 'data.csv'))
+//     .pipe(csv.parse(options))
+//     .on('error', (error) => {
+//       console.log(error);
+//     })
+//     .on('data', (row) => {
+//       // make id integer
+//       const id = Number(row.id);
+
+//       // make tags an array
+//       const tags: string[] = row.tags.split(',');
+//       row.tags = tags.map((el) => el.trim());
+
+//       _db[id] = row;
+//     })
+//     .on('end', (rowCount: number) => {
+//       // console.log(rowCount);
+//     });
+// }
+
+export default class TransactionRepository {
+  #db: any = {};
+
+  constructor(filename: string) {
+    const options: ParserOptionsArgs = {
+      objectMode: true,
+      delimiter: ',',
+      quote: '"',
+      headers: (headerArray) =>
+        headerArray.map((header: string | null | undefined) =>
+          header?.toLowerCase()
+        ),
+    };
+
+    // fs.createReadStream(path.resolve(__dirname, '../../data', 'data.csv'))
+    fs.createReadStream(path.resolve(__dirname, '../../data', filename))
+      .pipe(csv.parse(options))
+      .on('error', (error) => {
+        console.log(error);
+      })
+      .on('data', (row) => {
+        // make id integer
+        const id = Number(row.id);
+
+        // make tags an array
+        const tags: string[] = row.tags.split(',');
+        row.tags = tags.map((el) => el.trim());
+
+        this.#db[id] = row;
+      })
+      .on('end', (rowCount: number) => {
+        // console.log(rowCount);
+      });
+  }
+
+  list() {
+    return this.#db;
+  }
+
+  get() {
+    return this.#db;
+  }
+}
